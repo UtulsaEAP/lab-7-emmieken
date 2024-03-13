a2c9264d0753573ba7011ba256c80aad64e500b2
Emmie Kennemer <eek2654@utulsa.edu>
2024-03-13 13:11:19 -0500

    stuck on problem 3

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -1,5 +1,17 @@
 def wordInRange():
     #Type your code here
+    file_input = input()
+    word1 = input()
+    word2 = input()
+   
+    with open(file_input, 'r') as file:
+        inputs = file.readlines()
+        for i in inputs:
+            if word1[0:9] <= i[0:9] <= word2[0:9]:
+                print(i.strip() + " - in range")
+            else:
+                print(i.strip() + " - not in range")
+
     return
 if __name__ == '__main__':
     wordInRange()
--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -1,12 +1,35 @@
 def courseGrade():
     
     # TODO: Declare any necessary variables here. 
-      
-      
+    file = input().strip()
+    students = [] 
+    last_name = []
+    first_name = []
+    score1 = []
+    score2 = []
+    total_score = []
+
     # TODO: Read a file name from the user and read the tsv file here. 
-   
-   
+    with open(file, 'r') as file1:
+        contents = list(csv.reader(file1, delimeter=''))
+        for i in contents:
+            last_name = "".join(i[0])
+            first_name = "".join(i[1])
+            score1 = "".join(i[2])
+            score2 = "".join(i[3])
+            total_score = "".join(i[4])
+            
+            students.append(i)
+            last_name.append(last_name)
+            first_name.append(first_name)
+            score1.append(int(score1))
+            score2.append(int(score2))
+            total_score.append(int(total_score))
+            
+
     # TODO: Compute student grades and exam averages, then output results to a text file here. 
+    
+    
     return
 
 if __name__ == "__main__":
