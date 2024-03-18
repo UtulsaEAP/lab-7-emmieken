cc1245103f53408e8b64a4c615c5114c30f9d3a6
Emmie Kennemer <eek2654@utulsa.edu>
2024-03-18 11:59:51 -0500

    should all be done, not sure why prob 3 isnt passing even though it works

--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -6,6 +6,13 @@ Time: Friday at 3
 def fileNameChange():
     #type your code here
 
+    file_input = input()
+
+    with open(file_input, 'r') as f:
+        contents = f.read()
+        new_list = contents.replace('_photo.jpg', '_info.txt')
+        print(new_list)
+
     return
 
 if __name__ == '__main__':
